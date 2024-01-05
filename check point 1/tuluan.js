42bdf42c1b089817da9b505a4254a1b2fb769d2c
LongDragon14606 <140304779+LongDragon14606@users.noreply.github.com>
2024-01-05 19:03:46 +0700

    nộp bài check point 1

---
+++ check point 1/tuluan.js
@@ -0,0 +1,20 @@
+// Câu 9
+let m = [1,2,3,4,5,6,"hh","9",80,100]
+
+
+//Câu 10
+let a = "High knowlegde,high return"
+let arrMain = [a]
+console.log(...arrMain);
+
+//Câu 11
+NewPeople(name, age , address)
+    tên = this.name,
+    tuổi = this.age,
+    diachi = this.address
+
+NewStudent ()
+    name = this.name,
+    age = this.age,
+    address = this.address,
+    id = this.id 
